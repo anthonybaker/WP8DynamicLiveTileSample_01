58bdbcf2c470dc8dadebba1e4c42d0ebe1899235
Anthony Baker <abakerp@gmail.com>
2013-05-16 22:14:07 +0100

    Initial Commit.
    Adding solution files and external tile images.

---
+++ DynamicLiveTileSample_01/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DynamicLiveTileSample_01.Resources;
+
+namespace DynamicLiveTileSample_01
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
